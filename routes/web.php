05c99f9d418c01d6b85a84271d08489066fe1f88
Ashan Devinda <37576256+ashanda@users.noreply.github.com>
2023-11-29 09:47:40 +0530

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.auth.login');
 });
 
 Route::get('/login', [LoginController::class, 'userLoginForm'])->name('user.login-form');
