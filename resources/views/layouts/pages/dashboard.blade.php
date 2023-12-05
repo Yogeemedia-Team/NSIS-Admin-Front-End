47e7efc39bd706d2ca76aedfe72873261fdff8fb
Kasun24 <kasun961224@gmail.com>
2023-12-05 18:03:20 +0530

    dashboard layout

--- resources/views/layouts/pages/dashboard.blade.php
+++ resources/views/layouts/pages/dashboard.blade.php
@@ -66,7 +66,8 @@
            </li>
            <li class="nav-item px-3 d-flex align-items-center">
              <a href="javascript:;" class="nav-link text-body p-0">
-               <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
+               <!-- Hide gear icon -->
+               <!-- <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i> -->
              </a>
            </li>
            <li class="nav-item dropdown pe-2 d-flex align-items-center">
@@ -78,11 +79,11 @@
                  <a class="dropdown-item border-radius-md" href="javascript:;">
                    <div class="d-flex py-1">
                      <div class="my-auto">
-                       <img src="../../assets/img/team-2.jpg" class="avatar avatar-sm  me-3 " alt="user image">
+                       <img src="../../assets/img/ym.jpg" class="avatar avatar-sm  me-3 " alt="user image">
                      </div>
                      <div class="d-flex flex-column justify-content-center">
                        <h6 class="text-sm font-weight-normal mb-1">
-                         <span class="font-weight-bold">New message</span> from Laur
+                         <span class="font-weight-bold">New message</span> from Yogeemedia
                        </h6>
                        <p class="text-xs text-secondary mb-0">
                          <i class="fa fa-clock me-1"></i>
@@ -96,11 +97,11 @@
                  <a class="dropdown-item border-radius-md" href="javascript:;">
                    <div class="d-flex py-1">
                      <div class="my-auto">
-                       <img src="../../assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm bg-gradient-dark  me-3 " alt="logo spotify">
+                       <img src="../../assets/img/small_logo.png" class="avatar avatar-sm bg-gradient-dark  me-3 " alt="nsis">
                      </div>
                      <div class="d-flex flex-column justify-content-center">
                        <h6 class="text-sm font-weight-normal mb-1">
-                         <span class="font-weight-bold">New album</span> by Travis Scott
+                         <span class="font-weight-bold">New lecture</span> by NSIS
                        </h6>
                        <p class="text-xs text-secondary mb-0">
                          <i class="fa fa-clock me-1"></i>
@@ -153,22 +154,21 @@
          <div class="card">
            <div class="card-body p-3">
              <div class="row">
-               <div class="col-lg-9 my-auto">
+               <div class="col-md-7 col-lg-8 my-auto">
                  <div class="d-flex flex-column h-100">
-                   <p class="mb-1 pt-2 text-bold">Built by developers</p>
-                   <h5 class="font-weight-bolder">Soft UI Dashboard</h5>
-                   <p class="mb-5">From colors, cards, typography to complex elements, you will find the full documentation.</p>
+                   <h2 class="font-weight-bold">Dashboard</h2>
+                   <p class="mb-5">NSIS - School Management System</p>
                    <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
                      Read More
                      <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
                    </a>
                  </div>
                </div>
-               <div class="col-lg-3 ms-auto text-center mt-5 mt-lg-0">
-                 <div class="bg-gradient-primary border-radius-lg h-100">
+               <div class="col-md-5 col-lg-4 ms-auto text-center mt-5 mt-lg-0">
+                 <div class="border-radius-lg h-100">
                    <img src="../assets/img/shapes/waves-white.svg" class="position-absolute h-100 w-50 top-0 d-lg-block d-none" alt="waves">
                    <div class="position-relative d-flex align-items-center justify-content-center h-100">
-                     <img class="w-100 position-relative z-index-2 pt-4" src="../assets/img/illustrations/rocket-white.png" alt="rocket">
+                     <img class="w-100 position-relative z-index-2 pt-4" src="../assets/img/children.png" alt="rocket">
                    </div>
                  </div>
                </div>
@@ -182,10 +182,10 @@
        <div class="col-md-12">
          <div class="card z-index-2">
            <div class="p-3">
-             <h6>Sales overview</h6>
+             <h6>Attendance overview</h6>
              <p class="text-sm">
                <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
-               <span class="font-weight-bold">4% more</span> in 2021
+               <span class="font-weight-bold">4% more</span> in 2023
              </p>
            </div>
            <div class="card-body p-3">
@@ -203,15 +203,15 @@
              <div class="row">
                <div class="col-8">
                  <div class="numbers">
-                   <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Money</p>
+                   <p class="text-sm mb-0 text-capitalize font-weight-bold">Card 1 data</p>
                    <h5 class="font-weight-bolder mb-0">
-                     $53,000
-                     <span class="text-success text-sm font-weight-bolder">+55%</span>
+                     $11111
+                     <span class="text-success text-sm font-weight-bolder">+1%</span>
                    </h5>
                  </div>
                </div>
                <div class="col-4 text-end">
-                 <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                 <div class="icon icon-shape bg-gradient-secondary shadow text-center border-radius-md">
                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
                  </div>
                </div>
@@ -225,15 +225,15 @@
              <div class="row">
                <div class="col-8">
                  <div class="numbers">
-                   <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Users</p>
+                   <p class="text-sm mb-0 text-capitalize font-weight-bold">Card 2 data</p>
                    <h5 class="font-weight-bolder mb-0">
-                     2,300
-                     <span class="text-success text-sm font-weight-bolder">+3%</span>
+                     2222%
+                     <span class="text-success text-sm font-weight-bolder">+2%</span>
                    </h5>
                  </div>
                </div>
                <div class="col-4 text-end">
-                 <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                 <div class="icon icon-shape bg-gradient-danger shadow text-center border-radius-md">
                    <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
                  </div>
                </div>
@@ -247,15 +247,15 @@
              <div class="row">
                <div class="col-8">
                  <div class="numbers">
-                   <p class="text-sm mb-0 text-capitalize font-weight-bold">New Clients</p>
+                   <p class="text-sm mb-0 text-capitalize font-weight-bold">Card 3 data</p>
                    <h5 class="font-weight-bolder mb-0">
-                     +3,462
+                     +3333
                      <span class="text-danger text-sm font-weight-bolder">-2%</span>
                    </h5>
                  </div>
                </div>
                <div class="col-4 text-end">
-                 <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                 <div class="icon icon-shape bg-gradient-info shadow text-center border-radius-md">
                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
                  </div>
                </div>
@@ -269,15 +269,15 @@
              <div class="row">
                <div class="col-8">
                  <div class="numbers">
-                   <p class="text-sm mb-0 text-capitalize font-weight-bold">Sales</p>
+                   <p class="text-sm mb-0 text-capitalize font-weight-bold">Card 4 data</p>
                    <h5 class="font-weight-bolder mb-0">
-                     $103,430
+                     -4444
                      <span class="text-success text-sm font-weight-bolder">+5%</span>
                    </h5>
                  </div>
                </div>
                <div class="col-4 text-end">
-                 <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                 <div class="icon icon-shape bg-gradient-success shadow text-center border-radius-md">
                    <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
                  </div>
                </div>
@@ -286,7 +286,7 @@
          </div>
        </div>
      </div>
-     <footer class="footer pt-3  ">
+     <footer class="footer pt-5">
        <div class="container-fluid">
          <div class="row align-items-center justify-content-lg-between">
            <div class="col-lg-6 mb-lg-0 mb-4">
@@ -295,15 +295,13 @@
                  document.write(new Date().getFullYear())
                </script>,
                made with <i class="fa fa-heart"></i> by
-               <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+               <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Yogeemedia</a>
                for a better web.
              </div>
            </div>
            <div class="col-lg-6">
              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-               <li class="nav-item">
-                 <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
-               </li>
+
                <li class="nav-item">
                  <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
                </li>
@@ -320,76 +318,7 @@
      </footer>
    </div>
  </main>
- <!-- right sidebar -->
- <div class="fixed-plugin">
-   <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-     <i class="fa fa-cog py-2"> </i>
-   </a>
-   <div class="card shadow-lg ">
-     <div class="card-header pb-0 pt-3 ">
-       <div class="float-start">
-         <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
-         <p>See our dashboard options.</p>
-       </div>
-       <div class="float-end mt-4">
-         <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-           <i class="fa fa-close"></i>
-         </button>
-       </div>
-       <!-- End Toggle Button -->
-     </div>
-     <hr class="horizontal dark my-1">
-     <div class="card-body pt-sm-3 pt-0">
-       <!-- Sidebar Backgrounds -->
-       <div>
-         <h6 class="mb-0">Sidebar Colors</h6>
-       </div>
-       <a href="javascript:void(0)" class="switch-trigger background-color">
-         <div class="badge-colors my-2 text-start">
-           <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-           <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-           <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-           <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-           <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-           <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-         </div>
-       </a>
-       <!-- Sidenav Type -->
-       <div class="mt-3">
-         <h6 class="mb-0">Sidenav Type</h6>
-         <p class="text-sm">Choose between 2 different sidenav types.</p>
-       </div>
-       <div class="d-flex">
-         <button class="btn bg-gradient-primary w-100 px-3 mb-2 active" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-         <button class="btn bg-gradient-primary w-100 px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-       </div>
-       <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-       <!-- Navbar Fixed -->
 
-
-       <div class="mt-3">
-         <h6 class="mb-0">Navbar Fixed</h6>
-       </div>
-       <div class="form-check form-switch ps-0">
-         <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-       </div>
-       <hr class="horizontal dark my-sm-4">
-       <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/soft-ui-dashboard">Free Download</a>
-       <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/soft-ui-dashboard">View documentation</a>
-       <div class="w-100 text-center">
-         <a class="github-button" href="https://github.com/creativetimofficial/soft-ui-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
-         <h6 class="mt-3">Thank you for sharing!</h6>
-         <a href="https://twitter.com/intent/tweet?text=Check%20Soft%20UI%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fsoft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-           <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-         </a>
-         <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/soft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-           <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-         </a>
-       </div>
-     </div>
-   </div>
- </div>
- <!-- right sidebar -->
  @endsection
  @section('footer-scripts')
 
@@ -399,7 +328,7 @@
 
    var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
 
-   gradientStroke1.addColorStop(1, 'rgba(203,12,159,0.2)');
+   gradientStroke1.addColorStop(1, '#969FEE');
    gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
    gradientStroke1.addColorStop(0, 'rgba(203,12,159,0)'); //purple colors
 
@@ -418,7 +347,7 @@
            tension: 0.4,
            borderWidth: 0,
            pointRadius: 0,
-           borderColor: "#cb0c9f",
+           borderColor: "#2C3793",
            borderWidth: 3,
            backgroundColor: gradientStroke1,
            fill: true,
