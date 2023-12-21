cedce5cb977ad88013d46cfc5b9b409adb28ea2f
Kasun24 <kasun961224@gmail.com>
2023-12-21 15:47:17 +0530

    colors

--- resources/views/layouts/auth/login.blade.php
+++ resources/views/layouts/auth/login.blade.php
@@ -44,7 +44,7 @@
                       <label class="form-check-label" for="rememberMe">Remember me</label>
                     </div>
                     <div class="text-center">
-                      <button type="submit" class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
+                      <button type="submit" class="btn btn-lg bg-primary text-white btn-lg w-100 mt-4 mb-0">Sign in</button>
                     </div>
                   </form>
                 </div>
